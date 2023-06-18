b9ca484f054ba8b06d02d1b3be51469d4b44a56a
Madison Evans <m.corbinevans@gmail.com>
2023-06-18 11:00:31 -0700

    styling

---
+++ client/src/Components/DashboardLayout.tsx
@@ -0,0 +1,10 @@
+const DashboardLayout = () => {
+	return (
+		<div className="w-full h-full fixed bg-pink-500 flex flex-row">
+			<div className="bg-blue-400 min-w-[200px] w-[20%]">asdf</div>
+			<div className="bg-orange-300 w-full">asdf</div>
+		</div>
+	);
+};
+
+export default DashboardLayout;
